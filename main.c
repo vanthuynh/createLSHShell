9b61fa9048f0153ca82b079c6ed11ff0efa88557
Van Huynh <vthuynh@cpp.edu>
2021-12-02 09:00:36 +0000

    implementation of lsh_launch function

--- main.c
+++ main.c
@@ -1,3 +1,28 @@
+int lsh_launch(char **args)
+{
+    pid_t pid, wpid;
+    int status;
+
+    pid = fork();
+    if (pid == 0) {
+        // Child process
+        if (execvp(args[0], args) == -1) {
+            perror("lsh");
+        }
+        exit(EXIT_FAILURE);
+    } else if (pid < 0) {
+        // Error forking
+        perror("lsh");
+    } else {
+        // Parent process
+        do {
+            wpid = waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+
+    return 1;
+}
+
 // #define LSH_RL_BUFSIZE 1024
 char *lsh_read_line(void)
 {
