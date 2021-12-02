5596041772ca497a653582c1eedb82012940cc6d
Van Huynh <vthuynh@cpp.edu>
2021-12-02 08:44:34 +0000

    implementation of loop function

--- main.c
+++ main.c
@@ -1,3 +1,20 @@
+void lsh_loop(void)
+{
+  char *line;
+  char **args;
+  int status;
+
+  do {
+    printf("> ");
+    line = lsh_read_line();
+    args = lsh_split_line(line);
+    status = lsh_execute(args);
+
+    free(line);
+    free(args);
+  } while (status);
+}
+
 int main(int argc, char **argv)
 {
     // Load config files, if any.
